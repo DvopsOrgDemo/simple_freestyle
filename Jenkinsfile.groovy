ffb39f737826224a0e4a73289ce8bfb1c91feb46
Dvops-Demo <ashokthota90@gmail.com>
2022-03-16 02:56:47 +0530

    updated jenkins file

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,23 @@
+pipeline{
+    agent any
+    stages{
+        stage('git source'){
+            steps{
+                script{
+                     git clone https://github.com/DvopsOrgDemo/simple_freestyle.git
+                }
+                
+            }
+        }
+        stage('build'){
+            steps{
+                script{
+                    mvn clean install
+                }
+
+            }
+                
+        }
+        
+    }
+}
